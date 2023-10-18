309dfcbeb0708ed69853d375f235460f894e2533
minusno0708 <naoyao1014@gmail.com>
2023-10-19 02:58:59 +0900

    feat/venvファイルが無い場合に生成する

--- models/download.sh
+++ models/download.sh
@@ -1,2 +1,9 @@
-source venv/bin/activate
+if [ -d "venv" ]; then
+    source venv/bin/activate
+else
+    python -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+fi
+
 yolo task=detect mode=export model=yolov8n.pt format=onnx
