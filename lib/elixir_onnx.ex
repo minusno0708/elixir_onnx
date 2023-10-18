1c467878f8151a65503013143e6332995818a710
minusno0708 <naoyao1014@gmail.com>
2023-10-19 02:37:40 +0900

    build

---
+++ lib/elixir_onnx.ex
@@ -0,0 +1,18 @@
+defmodule ElixirOnnx do
+  @moduledoc """
+  Documentation for `ElixirOnnx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirOnnx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_onnx_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirOnnxTest do
+  use ExUnit.Case
+  doctest ElixirOnnx
+
+  test "greets the world" do
+    assert ElixirOnnx.hello() == :world
+  end
+end
